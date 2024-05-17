15e352c3926a12ac603f31d75a3b45f7d5f1b9ef
Ars1lan <sunnysheikh303@gmail.com>
2024-05-17 11:11:15 +0500

    Filters Logic Added for main Page

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -12,8 +12,8 @@
     @php
     $filters=[
     '' => 'Latest',
-    'pouplar_last_month' => 'Pouplar Last Month',
-    'pouplar_last_6months' => 'Pouplar Last 6 Months',
+    'popular_last_month' => 'Popular Last Month',
+    'popular_last_6months' => 'Popular Last 6 Months',
     'highest_rated_last_month' => 'Highest Rated Last Month',
     'highest_rated_last_6months' => 'Highest Rated Last 6 Months'
     ];
