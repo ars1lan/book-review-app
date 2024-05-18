4d8a6014d2a7b86983d9c3e113b896418aaf7ace
Ars1lan <sunnysheikh303@gmail.com>
2024-05-18 21:49:52 +0500

    Optimizing the database query by caching the data

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class BookController extends Controller
 {
@@ -27,7 +28,9 @@ class BookController extends Controller
             default => $books->latest()
         };
 
-        $books = $books->get();
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        $books = Cache::remember($cacheKey, 3600, fn () => $books->get());
+        // $books = $books->get();
         return view('books.index', ['books' => $books]);
     }
 
@@ -50,9 +53,11 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Book $book)
     {
-        //
+        $cacheKey = 'book:' . $book->id;
+        $book = Cache::remember($cacheKey, 3600, fn () => $book->load(['reviews' => fn ($query) => $query->latest()]));
+        return view('books.show', ['book' => $book]);
     }
 
     /**
